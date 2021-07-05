8555bd525c2e96c92516997c84553c25f8614a6b
eduardaaragao <eduardaaragao18@gmail.com>
2021-07-05 16:52:54 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 axios.defaults.baseURL = 'https://crowdzeroapi.herokuapp.com/'
